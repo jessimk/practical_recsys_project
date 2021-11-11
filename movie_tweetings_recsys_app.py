4ff5a851151c0214e6c0cb30ea5e9ec0576bf83f
Jes Simkin <42946705+jessimk@users.noreply.github.com>
2021-11-10 18:41:19 -0800

    a very basic streamlit app!

---
+++ movie_tweetings_recsys_app.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.read_json(
+    "https://raw.githubusercontent.com/jessimk/practical_recsys_project/main/top10charts.json")
+
+chosen_list = st.selectbox(
+        "Choose a List", list(df.columns),index=28)
+
+data=df[chosen_list]['movie_title']
+
+data=pd.DataFrame(data, index=['Title']).T
+data.index=np.arange(1,11,1)
+
+
+st.title('🍿 MovieTweetings Recommendations!')
+st.markdown('Movies recommendation are made using recent MovieTweetings ratings with a rating of 6 or higher.')
+
+st.markdown('MovieTweetings is a dataset of Twitter movie reviews: https://github.com/sidooms/MovieTweetings')
+
+st.write("### Top 10 "+chosen_list+" Movies", data)
